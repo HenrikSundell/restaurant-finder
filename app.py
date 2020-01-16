a827f3098eb7e4c56a2d89ef95aee6267530f9ca
henrik sundell <henrik.sundell@aalto.fi>
2020-01-16 22:03:23 +0200

    Add helper function for calculating distance between cordinates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,jsonify,request
 import json
+from math import sin, cos, atan2, pow, sqrt, radians
 
 app = Flask(__name__)
 
@@ -11,7 +12,21 @@ def index():
 
     return jsonify(data), 200
 
+#calculates distance between to cordinates
+#input two tuples with latitude and longitude of the cordinates
+#returns the distance between them in kilometers
+def get_distance(start, end):
+    start_lat, start_lon = radians(start[0]), radians(start[-1])
+    end_lat, end_lon = radians(end[0]), radians(end[-1])
 
+    lat_diff = start_lat - end_lat
+    lon_diff = start_lon - end_lon
+
+    a = pow(sin(lat_diff/2), 2) + cos(start_lat) * cos(end_lat) * pow(sin(lon_diff/2), 2)
+    earth_radius = 6371
+    distance = earth_radius * 2* atan2(sqrt(a), sqrt(1 - a))
+
+    return distance
 
 
 @app.route('/restaurants/search', methods=['GET'])
