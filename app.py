d9f961e64a695428909acff221593d357655dca8
henrik sundell <henrik.sundell@aalto.fi>
2020-01-16 21:14:12 +0200

    Get querrystring parameters

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Get restaurant data from jsonfile
+    with open('data/restaurants.json') as json_file:
+        data = json.load(json_file)
+
+    return jsonify(data), 200
+    return "HEllo"
+
+@app.route('/restaurants/search', methods=['GET'])
+def search():
+    tag = request.args.get('q')
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    # Get restaurant data from jsonfile
     with open('data/restaurants.json') as json_file:
         data = json.load(json_file)
     return jsonify(data), 200
